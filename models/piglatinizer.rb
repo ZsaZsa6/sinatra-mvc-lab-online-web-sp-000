2479d9321d9416cd022c050daa5174c5a7974bef
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 17:36:43 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,7 +12,7 @@ class PigLatinizer
             if ["a", "e", "i", "o", "u"].include?(word[2]) == false
               consonants << word[2]
             end
-      end
+          end
       "#{word[consonants.length..-1] + consonants.join + "ay"}"
     end
 
