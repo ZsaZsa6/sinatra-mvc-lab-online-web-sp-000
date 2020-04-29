564b7728606b13f35b9228a6359bccb5d8e1b4c4
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 16:35:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,14 +3,16 @@ class PigLatinizer
   def piglatinize(input_str)
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
-  def piglatinize_word(input_str)
-    if (input_str.match(/^[aeiou]/))
+  def piglatinize_word(word)
+    if (word.match(/^[aeiou]/))
       return input_str + "way"
-    else
+    else word[0]
     end
   end
-  def piglatinize_sentence(input_str)
-
-
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
+
+
+
 end
