ebc48f692e76bf21e5f1fbe2cb5f0507e582a458
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 16:13:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,5 +3,12 @@ class PigLatinizer
   def piglatinize(input_str)
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
-  
+  def piglatinize_word(input_str)
+    if (str.match(/^[aeiou]/)) return str + "way"
+    end
+  end
+  def piglatinize_sentence(input_str)
+    
+
+  end
 end
