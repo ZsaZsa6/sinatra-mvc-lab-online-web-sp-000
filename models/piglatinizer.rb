971711df5672aea35ffd26a8b55b966b6f6a9c44
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 17:17:04 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,12 +6,16 @@ class PigLatinizer
   def piglatinize_word(word)
     if word =~ (/\A[aeiou]/i)
       word = word + 'w'
-    elsif  
+    elsif
     end
   end
   def piglatinize_sentence(sentence)
     sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
-
+  
+  a = string.split(" ")
+    b = a.map {|word| piglatinize_word(word)}
+    b.join(" ")
+end
 
 end
