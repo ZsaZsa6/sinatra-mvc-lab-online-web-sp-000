7052708f6fd78b2a91b0477f10b9485e6702e737
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 16:38:39 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,7 +4,7 @@ class PigLatinizer
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
   def piglatinize_word(word)
-    if !consonant
+    if !consonant?(word[0])
        "way"
     else word[0]
     end
