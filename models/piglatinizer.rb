d3e49fc1125d476635684c03eca8601ab4519be3
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 17:26:45 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,12 +17,10 @@ class PigLatinizer
     end
 
   end
-
-  end
   def piglatinize(input_str)
     a = input_str.split(" ")
     b = a.map {|word| piglatinize_word(word)}
     b.join(" ")
-end
+  end
 
 end
