06a4015d3d6dcef03193515811054fc35aec97b2
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 17:39:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,8 +5,8 @@ class App < Sinatra::Base
     erb :user_input
   end
   post "/piglatinize" do
-    @user_inputphrase = params[:user_phrase]
-
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
   end
-
 end
