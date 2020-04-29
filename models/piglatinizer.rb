ea1959abf97d03f71564d8ab23d53d5fdc3d309d
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 17:25:57 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,7 @@ class PigLatinizer
   def piglatinize_word(word)
     first_letter = word[0].downcase
     if ["a", "e", "i", "o", "u"].include?(first_letter)
-        "#{word}way"
+        "#{word}ay"
     else
         consonants = []
         consonants << word[0]
