cc6d2710bb4c93c69ed109971c8fed272d9539df
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 17:15:16 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,10 +5,8 @@ class PigLatinizer
   end
   def piglatinize_word(word)
     if word =~ (/\A[aeiou]/i)
-      word = word + 'ay'
-    elsif word =~ (/\A[^aeiou]/i)
-      match = /\A[^aeiou]/i.match(word)
-      word = match.post_match + match.to_s + 'way'
+      word = word + 'w'
+    elsif  
     end
   end
   def piglatinize_sentence(sentence)
