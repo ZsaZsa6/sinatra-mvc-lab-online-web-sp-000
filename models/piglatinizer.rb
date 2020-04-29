1f545e88e4c219d906b2a5ca934ebc7f0a2cc492
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 17:24:51 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,8 +4,18 @@ class PigLatinizer
     first_letter = word[0].downcase
     if ["a", "e", "i", "o", "u"].include?(first_letter)
         "#{word}way"
-    elsif
-      
+    else
+        consonants = []
+        consonants << word[0]
+          if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+            consonants << word[1]
+            if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+              consonants << word[2]
+            end
+          end
+        "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    end
+
   end
 
   end
