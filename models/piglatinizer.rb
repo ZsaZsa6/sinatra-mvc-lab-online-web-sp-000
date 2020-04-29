dc2a0795549b88d1713c8ed0e287e913ea7f47aa
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-29 16:54:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,11 +4,13 @@ class PigLatinizer
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
   def piglatinize_word(word)
-    if !consonant?(word[0])
-       word = word  + "ay"
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word.split(" ")
+    if word =~ (/\A[aeiou]/i)
+      word = word + 'ay'
+    elsif word =~ (/\A[^aeiou]/i)
+      match = /\A[^aeiou]/i.match(word)
+      word = match.post_match + match.to_s + 'ay'
     end
+    word
   end
   def piglatinize_sentence(sentence)
     sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
